21e6bc996f07e8b4d753b9d89a9cc2c50f3de309
Hiru1003 <hirumiperera10@gmail.com>
2024-05-12 13:42:11 +0530

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Hirumi Perera on 2024-05-12.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
