9a9218c40513f91268d62cbb155bfecff90a298e
Hiru1003 <hirumiperera10@gmail.com>
2024-05-12 14:23:38 +0530

    feat: init

--- WeatherDemo/Manager/LocationManager.swift
+++ WeatherDemo/Manager/LocationManager.swift
@@ -13,4 +13,11 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     
     @Published var location: CLLocationCoordinate2D?
     @Published var isLoading = false
+    
+    override init() {
+        super.init()
+            
+        // Assigning a delegate to our CLLocationManager instance
+        manager.delegate = self
+    }
 }
