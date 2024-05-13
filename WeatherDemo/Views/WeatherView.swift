bb88116a3e8f6665467cb843577d2adc70592231
Hiru1003 <hirumiperera10@gmail.com>
2024-05-13 06:54:26 +0530

    feat: WeatherView

---
+++ WeatherDemo/Views/WeatherView.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherView.swift
+//  WeatherDemo
+//
+//  Created by Hirumi Perera on 2024-05-13.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var weather: ResponseBody
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView()
+    }
+}
